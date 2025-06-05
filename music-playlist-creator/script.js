3536605b786bcd8670c83a1fc4ab1711a431c042
Thomas Trivino <thomastrivino@meta.com>
2025-06-05 12:37:57 -0700

    Moves playlist data to data.js. Makes playlistcard size 225px. Adds heart icon to likes container.

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,79 +1,5 @@
 const playlistCards = document.getElementsByClassName("playlist-cards")[0];
 
-let playlistsData = [
-    {
-        playlistID: 1,
-        playlist_name: "Lov(sic.)",
-        playlist_author: "Shing02",
-        playlist_art: "https://picsum.photos/200",
-        songs: [
-            "Lov(sic.)pt1",
-            "Lov(sic.)pt2",
-            "Lov(sic.)pt3",
-            "Lov(sic.)pt4",
-            "Lov(sic.)pt5",
-            "Lov(sic.)pt6",
-            "Perfect Circle",
-        ],
-    },
-    {
-        playlistID: 2,
-        playlist_name: "Electronic Dreams",
-        playlist_author: "Tycho",
-        playlist_art: "https://picsum.photos/201",
-        songs: ["Awake", "Plunge", "For How Long", "Source", "Dive", "A Walk"],
-    },
-    {
-        playlistID: 3,
-        playlist_name: "Hip Hop Classics",
-        playlist_author: "DJ Premier",
-        playlist_art: "https://picsum.photos/202",
-        songs: [
-            "The World Is Yours",
-            "N.Y. State of Mind",
-            "The Message",
-            "Fight the Power",
-            "Straight Outta Compton",
-            "99 Problems",
-        ],
-    },
-    {
-        playlistID: 4,
-        playlist_name: "Lo-Fi Study Music",
-        playlist_author: "Jinsang",
-        playlist_art: "https://picsum.photos/203",
-        songs: ["Life", "Lost", "Found", " Serenity", "Peaceful", "Calm"],
-    },
-    {
-        playlistID: 5,
-        playlist_name: "Retro Video Games",
-        playlist_author: "Anamanaguchi",
-        playlist_art: "https://picsum.photos/204",
-        songs: [
-            "Airbrushed",
-            "Pitaya",
-            "Sakura",
-            "Kaleidoscope",
-            "Tetris",
-            "Pac-Man",
-        ],
-    },
-    {
-        playlistID: 6,
-        playlist_name: "Nature Sounds",
-        playlist_author: "Relaxing White Noise",
-        playlist_art: "https://picsum.photos/205",
-        songs: [
-            "Rainy Days",
-            "Ocean Waves",
-            "Forest Sounds",
-            "Babbling Brook",
-            "Wind Chimes",
-            "Crackling Fire",
-        ],
-    },
-];
-
 playlistsData.forEach((playlist) => {
     playlistCards.insertAdjacentHTML(
         "afterbegin",
@@ -83,7 +9,7 @@ playlistsData.forEach((playlist) => {
             <h2 class="playlist-title">${playlist.playlist_name}</h2>
             <p class="playlist-creator-name">${playlist.playlist_author}</p>
             <div class="playlist-likes-container">
-                <img class="playlist-heart-icon" src="assets/img/favicon.ico" alt="" width="20px" height="20px">
+                <p class="playlist-heart-icon">♡</p>
                 <p class="playlist-number-of-likes">0</p>
             </div>
         </section>
