d634bcc6c1d1361a8e07bc0b4fbcc3c78bf1a7a1
Thomas Trivino <thomastrivino@meta.com>
2025-06-05 09:55:40 -0700

    Adds modal with styling and a basic implementation of the JS logic.

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,19 @@
+// JavaScript for Opening and Closing the Modal
+const modal = document.getElementById("playlist-modal");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(playlist) {
+   document.getElementById('playlist-name').innerText = playlist.name;
+   document.getElementById('playlist-creator-name').innerText = playlist.creatorName;
+   document.getElementById('playlist-image').src = playlist.imageUrl;
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
